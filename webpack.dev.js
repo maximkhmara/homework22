31b1c003907db8a4b2192c0a9776e1b0891cf956
Max <maxcatalina@iMac-Max.local>
2024-05-21 17:06:16 +0300

    w

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
@@ -17,13 +16,10 @@ module.exports = {
 				use: [MiniCssExtractPlugin.loader, 'css-loader'],
 			},
 			{
-				test: /\.(jpe?g|png|gif|svg)$/i,
+				test: /\.(png|jpe?g|gif)$/i,
 				use: [
 					{
 						loader: 'file-loader',
-						options: {
-							name: '[path][name].[ext]',
-						},
 					},
 				],
 			},
@@ -35,46 +31,4 @@ module.exports = {
 		}),
 		new MiniCssExtractPlugin(),
 	],
-	optimization: {
-		minimizer: [
-			'...',
-			new ImageMinimizerPlugin({
-				minimizer: {
-					implementation: ImageMinimizerPlugin.imageminMinify,
-					options: {
-						// Lossless optimization with custom option
-						// Feel free to experiment with options for better result for you
-						plugins: [
-							['gifsicle', { interlaced: true }],
-							['jpegtran', { progressive: true }],
-							['optipng', { optimizationLevel: 5 }],
-							// Svgo configuration here https://github.com/svg/svgo#configuration
-							[
-								'svgo',
-								{
-									plugins: [
-										{
-											name: 'preset-default',
-											params: {
-												overrides: {
-													removeViewBox: false,
-													addAttributesToSVGElement: {
-														params: {
-															attributes: [
-																{ xmlns: 'http://www.w3.org/2000/svg' },
-															],
-														},
-													},
-												},
-											},
-										},
-									],
-								},
-							],
-						],
-					},
-				},
-			}),
-		],
-	},
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
@@ -17,64 +16,35 @@ module.exports = {
 				use: [MiniCssExtractPlugin.loader, 'css-loader'],
 			},
 			{
-				test: /\.(jpe?g|png|gif|svg)$/i,
+				test: /\.(gif|png|jpe?g|svg)$/i,
 				use: [
+					'file-loader',
 					{
-						loader: 'file-loader',
+						loader: 'image-webpack-loader',
 						options: {
-							name: '[path][name].[ext]',
+							mozjpeg: {
+								progressive: true,
+							},
+							// optipng.enabled: false will disable optipng
+							optipng: {
+								enabled: false,
+							},
+							pngquant: {
+								quality: [0.65, 0.9],
+								speed: 4,
+							},
+							gifsicle: {
+								interlaced: false,
+							},
+							// the webp option will enable WEBP
+							webp: {
+								quality: 75,
+							},
 						},
 					},
 				],
 			},
 		],
 	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: 'Development',
-		}),
-		new MiniCssExtractPlugin(),
-	],
-	optimization: {
-		minimizer: [
-			'...',
-			new ImageMinimizerPlugin({
-				minimizer: {
-					implementation: ImageMinimizerPlugin.imageminMinify,
-					options: {
-						// Lossless optimization with custom option
-						// Feel free to experiment with options for better result for you
-						plugins: [
-							['gifsicle', { interlaced: true }],
-							['jpegtran', { progressive: true }],
-							['optipng', { optimizationLevel: 5 }],
-							// Svgo configuration here https://github.com/svg/svgo#configuration
-							[
-								'svgo',
-								{
-									plugins: [
-										{
-											name: 'preset-default',
-											params: {
-												overrides: {
-													removeViewBox: false,
-													addAttributesToSVGElement: {
-														params: {
-															attributes: [
-																{ xmlns: 'http://www.w3.org/2000/svg' },
-															],
-														},
-													},
-												},
-											},
-										},
-									],
-								},
-							],
-						],
-					},
-				},
-			}),
-		],
-	},
+	plugins: [new HtmlWebpackPlugin(), new MiniCssExtractPlugin()],
 };
